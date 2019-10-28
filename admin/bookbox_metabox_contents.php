80e6bdabc0485e805e145a86d7793c835c59d0c3
John Beales <john@johnbeales.com>
2019-10-28 15:26:22 -0400

    Make sure bookbox metabox labels are always a line above the form field.

--- admin/bookbox_metabox_contents.php
+++ admin/bookbox_metabox_contents.php
@@ -9,21 +9,21 @@
 <p><?php _e('Info about this book used to display links to Amazon where your readers can buy the book.', 'deadtree'); ?></p>
 
 <p>
-	<label for="dt_bookbox_isbn"><?php _e('ISBN:', 'deadtree'); ?></label>
+	<label for="dt_bookbox_isbn"><?php _e('ISBN:', 'deadtree'); ?></label><br>
 	<input type="text" name="dt_bookbox_isbn" id="dt_bookbox_isbn" value="<?php echo esc_attr($rawdata['isbn']); ?>" />
 	<span class="howto"><abbr title="<?php _e('International Standard Book Number', 'deadtree'); ?>"><?php _e('ISBN', 'deadtree'); ?></abbr> <?php _e('(ISBN-10 or ISBN-13)', 'deadtree'); ?></span>
 </p>
 
 
 <p>
-	<label for="dt_bookbox_asin"><?php _e('Amazon US ASIN:', 'deadtree'); ?></label>
+	<label for="dt_bookbox_asin"><?php _e('Amazon US ASIN:', 'deadtree'); ?></label><br>
 	<input type="text" name="dt_bookbox_asin_amazon.com" id="dt_bookbox_asin_amazon.com" value="<?php echo esc_attr($rawdata['asin_amazon.com']); ?>" />
 	<span class="howto"><?php 
 	echo sprintf( __('The main %s for the book. This is used as a default for all Amazon sites.', 'deadtree'), $asin_html ); ?></span>
 </p>
 
 <p>
-	<label for="dt_bookbox_asin_ca"><?php _e('Amazon Canada ASIN:', 'deadtree'); ?></label>
+	<label for="dt_bookbox_asin_ca"><?php _e('Amazon Canada ASIN:', 'deadtree'); ?></label><br>
 	<input type="text" name="dt_bookbox_asin_amazon.ca" id="dt_bookbox_asin_amazon.ca" value="<?php echo esc_attr($rawdata['asin_amazon.ca']); ?>" />
 	<span class="howto"><?php
 	echo sprintf( __('The %s to use when sending readers Amazon.ca, if you want it to be different from Amazon.com', 'deadtree' ), $asin_html);
@@ -31,7 +31,7 @@
 </p>
 
 <p>
-	<label for="dt_bookbox_asin_uk"><?php _e('Amazon UK ASIN:', 'deadtree'); ?></label>
+	<label for="dt_bookbox_asin_uk"><?php _e('Amazon UK ASIN:', 'deadtree'); ?></label><br>
 	<input type="text" name="dt_bookbox_asin_amazon.co.uk" id="dt_bookbox_asin_amazon.co.uk" value="<?php echo esc_attr($rawdata['asin_amazon.co.uk']); ?>" />
 	<span class="howto"><?php
 	echo sprintf( __('The %s to use when sending readers Amazon.co, if you want it to be different from Amazon.com', 'deadtree'), $asin_html);
@@ -40,7 +40,7 @@
 
 
 <p>
-	<label for="dt_bookbox_comment"><?php _ex('Comment', 'Bookbox Metabox comment field label', 'deadtree'); ?></label>
+	<label for="dt_bookbox_comment"><?php _ex('Comment', 'Bookbox Metabox comment field label', 'deadtree'); ?></label><br>
 	<textarea rows="2" cols="33" type="text" name="dt_bookbox_comment" id="dt_bookbox_comment"><?php echo esc_textarea($rawdata['comment']); ?></textarea>
 	<span class="howto"><?php _e('Add a small message to the Bookbox if you wish.', 'deadtree'); ?></span>
 </p>
